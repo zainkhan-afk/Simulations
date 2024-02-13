531dccf436ae2117257318637bd5157f12e734c4
Zain Khan <zain.9496@gmail.com>
2024-02-13 18:37:05 +0500

    Added boids project

---
+++ Boids/Graphics.js
@@ -0,0 +1,11 @@
+class Graphics{
+	constructor(){
+
+	}
+
+	Draw(boids){
+		for (let  i = 0; i<boids.length; i++){
+			
+		}
+	}
+}
---
+++ Boids/mySketch.js
@@ -0,0 +1,15 @@
+const numBoids = 100;
+
+let graphics;
+let logic;
+let boids = [];
+function setup() 
+{
+	createCanvas(windowWidth, windowHeight);
+	
+}
+
+function draw()
+{
+	background(0);
+}
