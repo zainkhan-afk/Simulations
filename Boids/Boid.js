164280f2c7c04eea255cc323ddf239f4d73de282
Zain Khan <zain.9496@gmail.com>
2024-02-16 17:21:21 +0500

    Added code for boids, need to make some changes to get it working

---
+++ Boids/Boid.js
@@ -0,0 +1,21 @@
+class Boid{
+	constructor(pos, rotation, minVel, maxVel){
+		this.pos = pos;
+		this.vel = createVector(0, 0);
+		this.rotation = rotation
+
+		this.minVel = minVel;
+		this.maxVel = maxVel;
+	}
+
+	Update(deltaT){
+		// if(this.vel.mag() > this.maxVel){ 
+		// 	this.vel.setMag(this.maxVel);
+		// }
+		// if(this.vel.mag() < this.minVel){ 
+		// 	this.vel.setMag(this.minVel);
+		// }
+		this.rotation = this.vel.heading();
+		this.pos.add(p5.Vector.mult(this.vel, deltaT));
+	}
+}
--- Boids/mySketch.js
+++ Boids/mySketch.js
@@ -1,4 +1,16 @@
-const numBoids = 100;
+const numBoids = 250;
+const protectedRadius = 25;
+const visualRange = 100;
+const borderMargin = 100;
+
+const avoidFactor = 0.05;
+const alignmentFactor = 0.05;
+const centeringFactor = 0.005;
+const turnFactor = 0.02;
+
+const deltaT = 1;
+const maxVel = 7;
+const minVel = 3;
 
 let graphics;
 let logic;
@@ -6,10 +18,20 @@ let boids = [];
 function setup() 
 {
 	createCanvas(windowWidth, windowHeight);
-	
+
+
+	for (let i = 0; i<numBoids; i++){
+		let p = createVector(random(0, windowWidth), random(0, windowHeight));
+		append(boids, new Boid(p, 0, minVel, maxVel));
+	}
+
+	graphics = new Graphics();
+	logic = new Logic(protectedRadius, visualRange, borderMargin, avoidFactor, alignmentFactor, centeringFactor, turnFactor);
 }
 
 function draw()
 {
 	background(0);
+	graphics.Draw(boids);
+	logic.Step(boids, deltaT);
 }
