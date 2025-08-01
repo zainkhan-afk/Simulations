4010d6016ebb3b8506c7942450ef7f74630b1063
Zain <zain.9496@gmail.com>
2025-08-01 18:58:24 +0500

    Corner repulsion improved

--- Moths/moth.js
+++ Moths/moth.js
@@ -6,28 +6,40 @@ class Moth{
         this.acc = createVector(0, 0);
         this.maxSpeed = 3;
         this.maxForce = 0.1;
+        this.calcualteCorners();
 	}
+
+    calcualteCorners(){
+        this.xMin = 10;
+        this.xMax = windowWidth - 10;
+        this.yMin = 10;
+        this.yMax = windowHeight - 10;
+    }
 
     step()
     {
-        if (this.pos.x > windowWidth && this.acc.x > 0) {
-            this.acc.mult([-2.0, 0.0]);
+        if (this.pos.x > this.xMax && this.acc.x > 0) {
+            let val = this.pos.x - this.xMax;
+            this.acc.x = val* 0.5;
         }
 
-        else if (this.pos.x < 0 && this.acc.x < 0) {
-            this.acc.mult([-2.0, 0.0]);
+        else if (this.pos.x < this.xMin && this.acc.x < 0) {
+            let val = this.xMin - this.pos.x;
+            this.acc.x = val* 0.5;
         }
 
         // console.log(this.pos.y, windowHeight)
-        if (this.pos.y > windowHeight && this.acc.y > 0) {
-            this.acc.mult([0.0, -1.0]);
+        if (this.pos.y > this.yMax && this.acc.y > 0) {
+            let val = this.pos.y - this.yMax;
+            this.acc.y =  -val* 0.5;
         }
 
-        else if (this.pos.y < 0 && this.acc.y < 0) {
-            this.acc.mult([0.0, -1.0]);
+        else if (this.pos.y < this.yMin && this.acc.y < 0) {
+            let val = this.yMin - this.pos.y;
+            this.acc.y =  val * 0.5;
         }
         
-        this.acc.limit(this.maxForce);
+        // this.acc.limit(this.maxForce);
         this.vel.limit(this.maxSpeed);
         
         this.pos.add(this.vel);
