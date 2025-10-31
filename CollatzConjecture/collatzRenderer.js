b756317aee9e4250c7ced813789000c3e9685cee
Zain <zain.9496@gmail.com>
2025-10-31 12:04:15 +0500

    Added incremental logic

--- CollatzConjecture/collatzRenderer.js
+++ CollatzConjecture/collatzRenderer.js
@@ -1,24 +1,30 @@
 class CollatzRenderer{
 	constructor(pos){
         this.pos = pos;
-        this.angle = 0.1;
-        this.segmentLength = 10;
-
+        this.angle = 0.15;
+        this.segmentLength = 5;
+        
+        this.new_n = 1;
+        this.incremental_n_list = [];
+        this.incremental_angle_list = [];
+        this.line_complete = false;
+        this.incremental_n_list_idx = 0;
+        this.incremental_translate = createVector(0, 0);
+        this.incremental_rotation = 0;
 	}
 
     collatz(n){
         if (n % 2 == 0) { return int(n / 2); }
-        else { return (3*n + 1);}
+        else { return (3*n + 1) / 2;}
     }
 
     Render(){
         translate(this.pos.x, this.pos.y);
-        stroke(255, 100);
-        strokeWeight(1);
-        for (let i = 2; i < 10000; i++)
+        for (let i = 2; i < 3; i++)
         {
-            // let n = random(1);
-            let n = i;
+            let n = int(random(2, 100000));
+            // let n = i;
+            n = 20001;
             
             let allValues = [];
             while (n > 1){
@@ -32,10 +38,61 @@ class CollatzRenderer{
                 let n = allValues[j];
                 if (n%2 == 0) {rotate(this.angle);}
                 else {rotate(-this.angle);}
-                translate(0, -this.segmentLength);
-                line(0, 0, 0, this.segmentLength);
+                line(0, 0, this.segmentLength, 0);
+                translate(this.segmentLength, 0);
             }
             pop();
+        }
+    }
+
+    RenderIncremental(){
+        translate(this.pos.x, this.pos.y);
+        stroke(0, 50);
+        strokeWeight(1);
+
+        if (!this.line_complete)
+        {
+            // this.new_n = int(random(2, 100000));
+            let n = int(random(2, 100000));
+            // n = 20001;
+            // let n = i;
+            
+            while (n > 1){
+                let a = 0;
+                
+                if (n%2 == 0) {a = this.angle;}
+                else {a = -this.angle;}
+                
+                append(this.incremental_n_list, n);
+                append(this.incremental_angle_list, a);
+                
+                n = this.collatz(n);
+            }
+            append(this.incremental_n_list, 1);
+            append(this.incremental_angle_list, -this.angle);
+
+            this.line_complete = true;
+            this.incremental_n_list_idx = this.incremental_n_list.length - 1;
+        }
+        else{
+            for (let i = this.incremental_angle_list.length; i >= this.incremental_n_list_idx; i--){
+                let a = this.incremental_angle_list[i];
+                rotate(a);
+                translate(this.segmentLength, 0);
+            }
+            line(0, 0, this.segmentLength, 0);
+        }
+
+        this.incremental_n_list_idx -= 1;
+
+        if (this.incremental_n_list_idx <= 0){
+            this.incremental_n_list = [];
+            this.incremental_angle_list = [];
+            this.line_complete = false;
+            this.incremental_translate = this.pos.copy();
+            this.incremental_rotation = 0;
+            this.incremental_n_list_idx = 0;
+
         }
     }
 }
--- CollatzConjecture/mySketch.js
+++ CollatzConjecture/mySketch.js
@@ -1,17 +1,23 @@
-let pos;
 let collatzRenderer;
 function setup() 
-{
-	
+{	
     createCanvas(windowWidth, windowHeight);
-    collatzRenderer = new CollatzRenderer(createVector(windowWidth / 2, windowHeight / 2));
+    collatzRenderer = new CollatzRenderer(createVector(100, windowHeight / 2));
+    // stroke(10, 150);
+    // strokeWeight(1);
+    describe('A white circle on a gray background.');
+
+    background(0);
 }
 
 function draw()
 {
-    background(0);
-    collatzRenderer.Render();
-    noLoop();
+    push();
+    collatzRenderer.RenderIncremental();
+    pop();
+    // collatzRenderer.Render();
+    // noLoop();
+    // line(0, 0, 100, 100);
 }
 
 
